5048e12c89fb5c21d189b5ed34ca1fd6ca28421c
Sugene Mulpuri <sugenemulpuri9@gmail.com>
2024-02-02 17:59:59 -0600

    test phase 0 intro to js 2 looping code along

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+
+
+const names = [];
+
+function writeCards(names) {
+
+  for (let i = 0; i < names.length; i++) {
+    console.log(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    debugger;
+
+  }
+  return names;
+}
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+function countDown(number) {
+    let i = number;
+while (i >= 0) {
+  console.log(i--);
+}
+}
+
+countDown(11);
